94b593369f8e40c56a171cd65b1bc6cb471724e0
Kolja Lampe <razzeee@gmail.com>
2025-02-24 01:45:24 +0100

    Add margin

--- src/components/Treffs.tsx
+++ src/components/Treffs.tsx
@@ -2,8 +2,15 @@ import { nextTreff } from "@/chaostreff"
 import { format } from "date-fns"
 import { getLocale } from "../i18n/utils"
 import { Card, CardHeader, CardTitle, CardDescription } from "./ui/card"
+import { cn } from "@/lib/utils"
 
-export const Treffs = async ({ lang }: { lang: string }) => {
+export const Treffs = async ({
+  lang,
+  className,
+}: {
+  lang: string
+  className: string
+}) => {
   const treff = nextTreff
 
   if (!treff) {
@@ -11,7 +18,7 @@ export const Treffs = async ({ lang }: { lang: string }) => {
   }
 
   return (
-    <Card>
+    <Card className={cn(className)}>
       <CardHeader>
         <CardTitle>{treff.summary}</CardTitle>
         <CardDescription>
