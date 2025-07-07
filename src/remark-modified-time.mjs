978e89b860b9755357fb94b96a21e2cde362f290
eckynde <github@dunker-it.de>
2025-07-07 12:35:20 +0200

    iCal generation: fall back on fs timestamps if git timestamps are not available

--- src/remark-modified-time.mjs
+++ src/remark-modified-time.mjs
@@ -1,9 +1,11 @@
-import { execSync } from "child_process"
+import { execSync } from "child_process";
+import { statSync } from "fs";
 
 export function remarkModifiedTime() {
   return function (tree, file) {
-    const filepath = file.history[0]
-    const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`)
-    file.data.astro.frontmatter.lastModified = result.toString()
+    const filepath = file.history[0];
+    const gitresult = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`).toString();
+    const fsresult = statSync(filepath).mtime.toISOString();
+    file.data.astro.frontmatter.lastModified = gitresult ? gitresult : fsresult;
   }
 }
