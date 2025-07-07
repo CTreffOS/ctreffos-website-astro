80cf3f3deccff445d6c7974f776987d73fde0ecf
Jan Dunker <github@dunker-it.de>
2025-07-07 14:00:04 +0200

    Use nullish coalescing operator
    Co-authored-by: Kolja <razzeee@gmail.com>

--- src/remark-modified-time.mjs
+++ src/remark-modified-time.mjs
@@ -6,6 +6,6 @@ export function remarkModifiedTime() {
     const filepath = file.history[0];
     const gitresult = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`).toString();
     const fsresult = statSync(filepath).mtime.toISOString();
-    file.data.astro.frontmatter.lastModified = gitresult ? gitresult : fsresult;
+    file.data.astro.frontmatter.lastModified = gitresult ?? fsresult;
   }
 }
