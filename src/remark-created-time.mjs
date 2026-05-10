3dd84b84e823107de4baee86f22969cf19051b9e
Nils Israel <nils@sxda.io>
2026-05-10 13:43:47 +0200

    fix: correctly handle fs stats fallback for untracked files

--- src/remark-created-time.mjs
+++ src/remark-created-time.mjs
@@ -4,8 +4,8 @@ import { statSync } from "fs";
 export function remarkCreatedTime() {
   return function (tree, file) {
     const filepath = file.history[0];
-    const gitresult = execSync(`git log --pretty="format:%cI" "${filepath}" | tail -1`).toString();
+    const gitresult = execSync(`git log --pretty="format:%cI" "${filepath}" | tail -1`).toString().trim();
     const fsresult = statSync(filepath).birthtime.toISOString();
-    file.data.astro.frontmatter.created = gitresult ?? fsresult;
+    file.data.astro.frontmatter.created = gitresult || fsresult;
   }
 }
